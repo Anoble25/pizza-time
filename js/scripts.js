e17b7d393eafbd0fd479adb0250bbd9dde0d07da
Alex Noble <anobl22@wgu.edu>
2018-04-06 12:09:13 -0700

    add pizza cost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var cart=[];
 function Pizza (size, toppings){
   this.size=size;
   this.toppings=toppings;
+  this.cost=0;
 }
 
 function buildPizza(){
@@ -11,12 +12,33 @@ function buildPizza(){
 
 //Justin please don't copy my code
 
+Pizza.prototype.setCost = function() {
+  cost=0;
+  if (this.size==="small"){
+    cost=8;
+  } else if (this.size==="medium"){
+    cost=12;
+  } else if (this.size==="large"){
+    cost=14;
+  } else if (this.size==="extraLarge"){
+    cost=16;
+  } else{
+    console.log("pizza size error");
+  }
+
+  this.toppings.forEach(function(topping){
+    cost=cost+1;
+  });
+  this.cost=cost;
+}
+
   $("input:checkbox[name=toppings]:checked").each(function(){
     var topping = $(this).val();
     toppings.push(topping);
   });
 
   pizza1=new Pizza(size, toppings);
+  pizza1.setCost();
   cart.push(pizza1);
 }
 
